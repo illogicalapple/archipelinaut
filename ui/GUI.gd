556c3ddc4ad0deb01e63dc6f34470e292ce5dfc2
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2023-11-23 11:04:48 -0600

    snazzy pause effect

--- ui/GUI.gd
+++ ui/GUI.gd
@@ -5,20 +5,29 @@ var health = 100
 var loading_progress: float = 0: set = set_loading_progress
 var cursor = preload("../cursor.svg")
 
+@onready var parent = get_parent()
+
 func pause(event: InputEvent):
-	if event.is_action_pressed("ui_pause"):
-		paused = !paused
-		$Control/Pause/Default/Options.open_screen($Control/Pause/Default)
-		if paused:
-			get_tree().set_deferred("paused", true)
-			Input.set_custom_mouse_cursor(null)
-			$Control/Pause.show()
-			return
-		for child in $Control/Pause/Default/Options.get_children():
-			(child as Label).add_theme_font_size_override("font_size", 56)
-		get_tree().paused = false
-		$Control/Pause.hide()
-		Input.set_custom_mouse_cursor(cursor, 0, Vector2(16, 16))
+	if not event.is_action_pressed("ui_pause"): return
+	
+	paused = not paused
+	$Control/Pause/Default/Options.open_screen($Control/Pause/Default)
+	if paused:
+		get_tree().set_deferred("paused", true)
+		$Control/BGBlur.show()
+		Input.set_custom_mouse_cursor(null)
+		$Control/Pause.show()
+		AudioServer.set_bus_effect_enabled(1, 0, true)
+		AudioServer.set_bus_volume_db(1, -7)
+		return
+	for child in $Control/Pause/Default/Options.get_children():
+		(child as Label).add_theme_font_size_override("font_size", 56)
+	$Control/BGBlur.hide()
+	AudioServer.set_bus_effect_enabled(1, 0, false)
+	AudioServer.set_bus_volume_db(1, 0)
+	get_tree().paused = false
+	$Control/Pause.hide()
+	Input.set_custom_mouse_cursor(cursor, 0, Vector2(16, 16))
 
 func _ready():
 	var hints = $Control/Loading/Hint.get_meta("hints")
